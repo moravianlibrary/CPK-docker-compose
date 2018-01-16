6212097ce53defb0139f75ac2e87c9b977e0f933
Vaclav Rosecky <xrosecky@gmail.com>
2018-01-16 08:16:43 +0100

    EDS.ini support when bootstrapping form source

--- builds/cpk-source/bootstrap-source.sh
+++ builds/cpk-source/bootstrap-source.sh
@@ -4,8 +4,7 @@ perror() {
     echo "$@" >&2
 }
 
-main() {
-
+init_config_local() {
     CONFIG_LOCAL="${PARAM_VUFIND_SRC}/local/config/vufind/config.local.ini"
     if [ -f "$CONFIG_LOCAL" ]; then
         return 0
@@ -13,18 +12,31 @@ main() {
 
     cp /tmp/config.local.template.ini "$CONFIG_LOCAL"
     sed -i \
-      -e "s#PARAM_VUFIND_DEBUG#${PARAM_VUFIND_DEBUG:-false}#g" \
-      -e "s#PARAM_VUFIND_CAPTCHA_SITE_KEY#${PARAM_VUFIND_CAPTCHA_SITE_KEY}#g" \
-      -e "s#PARAM_VUFIND_CAPTCHA_SECRET_KEY#${PARAM_VUFIND_CAPTCHA_SECRET_KEY}#g" \
-      -e "s#PARAM_VUFIND_URL#${PARAM_VUFIND_URL}#g" \
-      -e "s#PARAM_VUFIND_SOLR_URL#${PARAM_VUFIND_SOLR_URL}#g" \
-      -e "s#PARAM_VUFIND_SOLR_INDEX#${PARAM_VUFIND_SOLR_INDEX}#g" \
-      -e "s#PARAM_VUFIND_MYSQL_URL#${PARAM_VUFIND_MYSQL_URL}#g" \
-      -e "s#PARAM_VUFIND_GOOGLE_API_KEY#${PARAM_VUFIND_GOOGLE_API_KEY}#g" \
-      -e "s#PARAM_VUFIND_INFO_KNIHOVNY#${PARAM_VUFIND_INFO_KNIHOVNY}#g" \
-      "$CONFIG_LOCAL"
-
+        -e "s#PARAM_VUFIND_DEBUG#${PARAM_VUFIND_DEBUG:-false}#g" \
+        -e "s#PARAM_VUFIND_CAPTCHA_SITE_KEY#${PARAM_VUFIND_CAPTCHA_SITE_KEY}#g" \
+        -e "s#PARAM_VUFIND_CAPTCHA_SECRET_KEY#${PARAM_VUFIND_CAPTCHA_SECRET_KEY}#g" \
+        -e "s#PARAM_VUFIND_URL#${PARAM_VUFIND_URL}#g" \
+        -e "s#PARAM_VUFIND_SOLR_URL#${PARAM_VUFIND_SOLR_URL}#g" \
+        -e "s#PARAM_VUFIND_SOLR_INDEX#${PARAM_VUFIND_SOLR_INDEX}#g" \
+        -e "s#PARAM_VUFIND_MYSQL_URL#${PARAM_VUFIND_MYSQL_URL}#g" \
+        -e "s#PARAM_VUFIND_GOOGLE_API_KEY#${PARAM_VUFIND_GOOGLE_API_KEY}#g" \
+        -e "s#PARAM_VUFIND_INFO_KNIHOVNY#${PARAM_VUFIND_INFO_KNIHOVNY}#g" \
+        "$CONFIG_LOCAL"
 }
 
-main "$@"
+init_eds_config() {
+    CONFIG_EDS="${PARAM_VUFIND_SRC}/local/config/vufind/EDS.ini"
+    if [ -z "$PARAM_VUFIND_EDS_LOGIN" ]; then
+        return 0
+    fi;
+
+    sed -i \
+        -e "s#PARAM_VUFIND_EDS_LOGIN#${PARAM_VUFIND_EDS_LOGIN}#g" \
+        -e "s#PARAM_VUFIND_EDS_PASSWD#${PARAM_VUFIND_EDS_PASSWD}#g" \
+        -e "s#PARAM_VUFIND_EDS_PROFILE#${PARAM_VUFIND_EDS_PROFILE}#g" \
+        "$CONFIG_EDS"
+}
+
+init_config_local "$@"
+init_eds_config "$@"
 exit $?
