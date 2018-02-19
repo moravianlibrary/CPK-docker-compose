5f64c758779f3942f0f0415e4ed8a55719a083b0
Vaclav Rosecky <xrosecky@gmail.com>
2018-02-19 09:25:11 +0100

    compile CSS styles when building from source

--- builds/cpk-source/cpk-clone.sh
+++ builds/cpk-source/cpk-clone.sh
@@ -8,4 +8,6 @@ if [ ! -z "$PARAM_VUFIND_BRANCH" ]; then
     git clone --depth 1 -b "$PARAM_VUFIND_BRANCH" "https://github.com/moravianlibrary/CPK.git" "$PARAM_VUFIND_SRC"
     mkdir -p "$PARAM_VUFIND_SRC/log"
     chown www-data:www-data "$PARAM_VUFIND_SRC/log"
+    cd "$PARAM_VUFIND_SRC"
+    php util/cssBuilder.php
 fi;
