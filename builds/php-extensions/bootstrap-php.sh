9bb200c55a9bd86e889fd204d9f4797fd4fca8d2
Vaclav Rosecky <xrosecky@gmail.com>
2018-05-11 07:04:25 +0200

    opcache can be disabled

--- builds/php-extensions/bootstrap-php.sh
+++ builds/php-extensions/bootstrap-php.sh
@@ -4,6 +4,9 @@ main() {
     if [ "$PARAM_XDEBUG_ENABLED" == true ]; then
         docker-php-ext-enable xdebug
     fi;
+    if [ "$PARAM_AGGRESSIVE_OPCACHE" == true ]; then
+        echo "opcache.validate_timestamps=0\n" >> /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
+    fi;
 }
 
 main "$@"
