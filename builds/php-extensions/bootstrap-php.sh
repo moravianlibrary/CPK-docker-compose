52ecdb20caec23689e7719405846b198de42ef58
Vaclav Rosecky <xrosecky@gmail.com>
2019-07-30 09:51:26 +0200

    feature-1225: Xdebug profiler can be enabled

--- builds/php-extensions/bootstrap-php.sh
+++ builds/php-extensions/bootstrap-php.sh
@@ -3,9 +3,20 @@
 main() {
     if [ "$PARAM_XDEBUG_ENABLED" == true ]; then
         docker-php-ext-enable xdebug
+        if [ ! -z "$PARAM_XDEBUG_PROFILER_TRIGGER_VALUE" ]; then
+            PROFILER_OUTPUT_DIRECTORY="/var/www/cpk/profiler"
+            if [ ! -d "$PROFILER_OUTPUT_DIRECTORY" ]; then
+                mkdir "$PROFILER_OUTPUT_DIRECTORY"
+                chown www-data:www-data "$PROFILER_OUTPUT_DIRECTORY"
+            fi;
+            echo "xdebug.profiler_enable=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+            echo "xdebug.profiler_enable_trigger=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+            echo "xdebug.profiler_enable_trigger_value=$PARAM_XDEBUG_PROFILER_TRIGGER_VALUE" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+            echo "xdebug.profiler_output_dir=/var/www/cpk/profiler" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+        fi;
     fi;
     if [ "$PARAM_AGGRESSIVE_OPCACHE" == true ]; then
-        echo "opcache.validate_timestamps=0\n" >> /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
+        echo "opcache.validate_timestamps=0" >> /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
     fi;
 }
 
